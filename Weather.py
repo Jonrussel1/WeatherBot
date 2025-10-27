a627c6e09ce1398d2e4b04b7fd94ad2276b38875
T <JAYSONC678@GMAIL.COM>
2025-10-27 16:38:50 -0600

    Prints Location and Temp also

--- Weather.py
+++ Weather.py
@@ -22,8 +22,10 @@ def coords_to_gridpoints(coordinates):
     coordinate_endpoint = f"{BASE_URL}/points/{coordinates[0]},{coordinates[1]}"
 
     coordinate_data = get_data_as_json(coordinate_endpoint, headers)["properties"]
-    station_and_coords = [coordinate_data["gridId"], coordinate_data["gridX"], coordinate_data["gridY"]]
-    return station_and_coords
+    location_data = coordinate_data["relativeLocation"]["properties"]
+    location = f"{location_data["city"]}, {location_data["state"]}"
+    station__coords_and_location = (coordinate_data["gridId"], coordinate_data["gridX"], coordinate_data["gridY"], location)
+    return station__coords_and_location
 
 def forecast_from_gridpoints(station_and_coords):
     forecast_endpoint = f"{BASE_URL}/gridpoints/{station_and_coords[0]}/{station_and_coords[1]},{station_and_coords[2]}/forecast"
@@ -32,7 +34,8 @@ def forecast_from_gridpoints(station_and_coords):
 
     #save the forecast in lowercase
     weather = forecast_data["shortForecast"].lower()
-    return weather
+    temp = forecast_data["temperature"]
+    return (weather, temp, station_and_coords[3])
 
 def get_weather(coords = [32.9004,-105.9629]):
-    return forecast_from_gridpoints(coords_to_gridpoints(coords))
+    return (forecast_from_gridpoints(coords_to_gridpoints(coords)))
