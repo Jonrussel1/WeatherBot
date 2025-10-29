5cab020e2b180fe46cb0a456705d26a28dc12312
soggynachos <angel13asdf@gmail.com>
2025-10-29 16:23:04 -0600

    moved api into a class to class format

--- Weather.py
+++ Weather.py
@@ -1,16 +1,25 @@
+from tkinter import *
+import tkinter as tk
 import requests
 import random
 
 
-#api setup
-user_agent = "jaysonc678@gmail.com"
-#needed to access api
-BASE_URL = "https://api.weather.gov"
-headers = {'User-Agent' : f'{user_agent}'}
 
-class Get_weather:
+class Get_weather(Tk):
+    
+
     def __init__(self):
         super().__init__()
+
+         #title, icon, size
+        self.title('WeatherBot')
+        self.geometry("800x600")
+        self.configure(bg='teal')
+        #api setup
+        self.user_agent = "jaysonc678@gmail.com"
+        #needed to access api
+        self.BASE_URL = "https://api.weather.gov"
+        self.headers = {'User-Agent' : f'{self.user_agent}'}
 
     def get_data_as_json(self,endpoint, headers):
         self.response = requests.get(endpoint, headers = headers) #raise error if api failed
@@ -22,16 +31,16 @@ class Get_weather:
 
 
     def coords_to_gridpoints(self,coordinates):
-        self.coordinate_endpoint = f"{BASE_URL}/points/{coordinates[0]},{coordinates[1]}"
+        self.coordinate_endpoint = f"{self.BASE_URL}/points/{coordinates[0]},{coordinates[1]}"
 
-        self.coordinate_data = self.get_data_as_json(self.coordinate_endpoint, headers)["properties"]
+        self.coordinate_data = self.get_data_as_json(self.coordinate_endpoint, self.headers)["properties"]
         self.station_and_coords = [self.coordinate_data["gridId"], self.coordinate_data["gridX"], self.coordinate_data["gridY"]]
         return self.station_and_coords
 
     def forecast_from_gridpoints(self,station_and_coords):
-        self.forecast_endpoint = f"{BASE_URL}/gridpoints/{station_and_coords[0]}/{station_and_coords[1]},{station_and_coords[2]}/forecast"
+        self.forecast_endpoint = f"{self.BASE_URL}/gridpoints/{station_and_coords[0]}/{station_and_coords[1]},{station_and_coords[2]}/forecast"
         #get info from api then get specifically the first period
-        self.forecast_data = self.get_data_as_json(self.forecast_endpoint, headers)["properties"]["periods"][0]
+        self.forecast_data = self.get_data_as_json(self.forecast_endpoint, self.headers)["properties"]["periods"][0]
 
         #save the forecast in lowercase
         self.weather = self.forecast_data["shortForecast"].lower()
@@ -43,3 +52,6 @@ class Get_weather:
 
 #create an instance of class = object
 weather = Get_weather()
+weather.get_weather()
+
+weather.mainloop()
