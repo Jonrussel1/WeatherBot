f25d69b1f7b78202fc0a339f3646e27dad445a42
T <JAYSONC678@GMAIL.COM>
2025-10-29 16:33:50 -0600

    Almost there

--- class_approach_WeatherBot.py
+++ class_approach_WeatherBot.py
@@ -8,7 +8,6 @@ class Weather_Main_Window(Tk):
     
     def __init__(self):
         super().__init__()
-        self.weather = Weather.Get_Weather()
         # to get the location of the current python file
         self.basedir = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,45 +15,60 @@ class Weather_Main_Window(Tk):
         self.title('WeatherBot')
         self.geometry("800x600")
         self.configure(bg='teal')
+        self.weather = Weather.Get_Weather()
        
 
         #label
         self.my_label = Label(self, text='Welcome to WeatherBot', font=('Helvetica', 42))
         self.my_label.pack(pady=20)
+        self.Suggestion_button()
+        self.Setting_Button()
+        self.Todo_Button()
+        self.Weather_Button()
+        self.Exit_button()
     
-    def Get_Weather(self, coords):
-        return self.weather.get_weather(coords)
     
     def Weather_Button(self):
-        self.weather_button = Button(text="Get Weather Info", command=Weather_Window, bg="#333", fg="white", bd=0)
+        self.weather_button = Button(self, text="Get Weather Info", command=self.Open_Weather_Window, bg="#333", fg="white", bd=0)
         self.weather_button.pack(pady=30)
         
     def Todo_Button(self):
-        self.todo_button = Button(text='To Do list', command=Todo_Window, bg='#333',fg='white',bd=0)
+        self.todo_button = Button(self, text='To Do list', command=self.Open_ToDo_Window, bg='#333',fg='white',bd=0)
         self.todo_button.pack(pady=50)
     
     def Setting_Button(self):
         self.img = PhotoImage(file=self.basedir+"/gear.png").subsample(3, 3)
         self.menu = Menu(self, tearoff=0, bg="red", fg="black")
         self.setting_button = Button(self, image=self.img, bd=0, relief="flat", highlightthickness=0,
-                bg="teal", activebackground="teal", command=Setting_Window)
+                bg="teal", activebackground="teal", command=self.Open_Settings_Window)
         self.setting_button.place(relx=1.0, rely=1.0, x=-10, y=-10, anchor="se")
    
-
+    def Get_Weather(self, coords):
+        return self.weather.get_weather(coords)
+    
     def Suggestion_button(self):
-        self.suggest_button = Button(text='Recommend',command=Suggestion_Window,bg='#333',fg='white',bd=0)
+        self.suggest_button = Button(self, text='Recommend',command=self.Open_Suggestion_Window,bg='#333',fg='white',bd=0)
         self.suggest_button.pack(pady=60)
  
     def Exit_button(self):
-        self.exit_button = Button(text='Exit',command=self.destroy, bg='#333',fg='white',bd=0)
+        self.exit_button = Button(self, text='Exit',command=self.destroy, bg='#333',fg='white',bd=0)
         self.exit_button.pack(pady=65)
-
-class Weather_Window(Tk):
-   
-    def __init__(self): 
-        super().__init__()
-
+    
+    def Open_Weather_Window(self):
+        Weather_Window(self)
+    def Open_ToDo_Window(self):
+        Todo_Window(self)
+    def Open_Settings_Window(self):
+        Setting_Window(self)
         
+    def Open_Suggestion_Window(self):
+        Suggestion_Window(self)
+class Weather_Window(Toplevel):
+   
+    def __init__(self, master): 
+        super().__init__(master)
+
+        self.master = master
         #title, icon, size
         self.title('Weather information')
         self.geometry("800x600")
@@ -77,14 +91,14 @@ class Weather_Window(Tk):
         q = StringVar()
         Entry(self.topbar, textvariable=q, font=("TkDefaultFont",12)).pack(side="left", padx=8, pady=8, fill="x", expand=True)
         Button(self.topbar, text="Search", bg="#333", fg="white", bd=0,
-              command=lambda: q.set(Weather_Main_Window.Get_Weather(32.9004,-105.9629))).pack(side="left", padx=6)
+              command=lambda: q.set(self.master.Get_Weather(self.my_label.split(',')))).pack(side="left", padx=6)
     
     
-class Todo_Window(Tk):
+class Todo_Window(Toplevel):
     
-    def __init__(self): 
+    def __init__(self, master): 
         
-        super().__init__()
+        super().__init__(master)
 
         
          #title, icon, size
@@ -93,10 +107,10 @@ class Todo_Window(Tk):
         self.configure(bg='teal')
 
 
-class Setting_Window(Tk):
+class Setting_Window(Toplevel):
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, master):
+        super().__init__(master)
      
         self.title('Settings')
         self.geometry('400x400')
@@ -112,9 +126,9 @@ class Setting_Window(Tk):
         self.Slider.set(value=10)
         self.Slider.pack(padx=10, pady=2, fill="x")
 
-class Suggestion_Window(Tk):
+class Suggestion_Window(Toplevel):
     
-    def __init__(self):
+    def __init__(self, master):
 
         super().__init__()
             #title, icon, size
@@ -252,15 +266,6 @@ class Suggestion_Window(Tk):
 
 #instantiation
 app = Weather_Main_Window()
-
-
-
-app.Weather_Button()
-app.Todo_Button()
-app.Setting_Button()
-app.Suggestion_button()
-app.Exit_button()
-
 
 
 
